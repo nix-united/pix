9c05e49d24c71670ce386dd4868f26976a050120
Mykyta Pidhornyi <mykyta.pidhornyi@nixs.com>
2023-08-14 19:15:51 +0300

    fix: env in workflow and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,40 +4,19 @@ import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
 export default defineConfig(({ command }) => {
-    if (command === 'serve') {
-        return {
-            plugins: [react(), svgr()],
-            server: {
-                port: 8080
-            },
-            resolve: {
-                alias: {
-                    '@': resolve(__dirname, './src'),
-                    '@components': resolve(__dirname, './src/components'),
-                    '@pages': resolve(__dirname, './src/pages'),
-                    '@assets': resolve(__dirname, './src/assets'),
-                    '@hooks': resolve(__dirname, './src/hooks'),
-                    '@utils': resolve(__dirname, './src/utils'),
-                    '@types': resolve(__dirname, './src/types'),
-                    '@constants': resolve(__dirname, './src/constants')
-                }
-            }
-        }
-    } else {
-        return {
-            plugins: [react(), svgr()],
-            base: '/pix/',
-            resolve: {
-                alias: {
-                    '@': resolve(__dirname, './src'),
-                    '@components': resolve(__dirname, './src/components'),
-                    '@pages': resolve(__dirname, './src/pages'),
-                    '@assets': resolve(__dirname, './src/assets'),
-                    '@hooks': resolve(__dirname, './src/hooks'),
-                    '@utils': resolve(__dirname, './src/utils'),
-                    '@types': resolve(__dirname, './src/types'),
-                    '@constants': resolve(__dirname, './src/constants')
-                }
+    return {
+        plugins: [react(), svgr()],
+        base: command === 'serve' ? '/' : '/pix/',
+        resolve: {
+            alias: {
+                '@': resolve(__dirname, './src'),
+                '@components': resolve(__dirname, './src/components'),
+                '@pages': resolve(__dirname, './src/pages'),
+                '@assets': resolve(__dirname, './src/assets'),
+                '@hooks': resolve(__dirname, './src/hooks'),
+                '@utils': resolve(__dirname, './src/utils'),
+                '@types': resolve(__dirname, './src/types'),
+                '@constants': resolve(__dirname, './src/constants')
             }
         }
     }
